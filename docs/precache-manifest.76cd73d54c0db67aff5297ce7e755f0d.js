e51b69d697c2f50df967dc04e387cd336fc7db81
aungkyawhein <akh.infotech@gmail.com>
2020-05-27 23:06:34 +0800

    add docs folder and remove lint on commit

---
+++ docs/precache-manifest.76cd73d54c0db67aff5297ce7e755f0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    revision: 'd02011e100e5528efde5',
+    url: 'css/app.dca5900b.css',
+  },
+  {
+    revision: '82b9c7a5a3f405032b1db71a25f67021',
+    url: 'img/logo.82b9c7a5.png',
+  },
+  {
+    revision: '7ade7edb0ffc9285a7fbc59d81921f47',
+    url: 'index.html',
+  },
+  {
+    revision: 'd02011e100e5528efde5',
+    url: 'js/app.b54e6744.js',
+  },
+  {
+    revision: '66bd655c3403f8cd7585',
+    url: 'js/chunk-vendors.a91d8644.js',
+  },
+  {
+    revision: '3fcbc1a15fc18a6b4d1b5b4896369ad6',
+    url: 'manifest.json',
+  },
+  {
+    revision: 'b6216d61c03e6ce0c9aea6ca7808f7ca',
+    url: 'robots.txt',
+  },
+]);
---
+++ vue.config.js
@@ -0,0 +1,6 @@
+// vue.config.js
+module.exports = {
+  publicPath: './',
+  outputDir: 'docs',
+  lintOnSave: process.env.NODE_ENV !== 'production',
+};
